319b636da733362585100b2caa77b94e0d1b4eea
John Li <johnsli@msn.com>
2013-09-12 13:19:48 -0400

    Initializing Gitignore for Eventinfo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EventInfo::Application.config.secret_key_base = 'ac7d58e1711730fd3b91bbbe3e82e9eba4558037bb52f5093e0a833770e7945dd1b355947719494cbfd59748a4f261e8fd1ba7e619aecc09a7c538baab6730b9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EventInfo::Application.config.session_store :cookie_store, key: '_EventInfo_session'
