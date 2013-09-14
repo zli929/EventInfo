9ef2eebf9a3744ab6124afe77b6076ab1fdf82f6
John Li <johnsli@msn.com>
2013-09-14 10:48:26 -0400

    Begin building out new item forms and feed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,16 +7,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   private
-  
     def authenticate_user_from_token!
       
     user_token = params[:auth_token].presence
     user       = user_token && User.find_by_authentication_token(user_token)
  
       if user
-        # Notice how we use Devise.secure_compare to compare the token
-        # in the database with the token given in the params, mitigating
-        # timing attacks.
         sign_in user, store: false
       end
     end
