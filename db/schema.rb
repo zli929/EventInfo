1d17a59279ddb2a678fa14f78d0698436eb96fea
John Li <johnsli@msn.com>
2013-10-14 15:50:42 -0400

    Fixed user sign in redirect issues

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131008205358) do
+ActiveRecord::Schema.define(version: 20131013043747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,6 +67,8 @@ ActiveRecord::Schema.define(version: 20131008205358) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "authentication_token"
+    t.string   "network_email"
+    t.string   "unconfirmed_email"
   end
 
   add_index "users", ["authentication_token"], name: "index_users_on_authentication_token", unique: true, using: :btree
