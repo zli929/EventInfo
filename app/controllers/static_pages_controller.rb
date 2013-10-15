b2a0a7b2445e6b4876738b6c2bc0f2c09ed54882
John Li <johnsli@msn.com>
2013-10-15 00:10:42 -0400

    Rolling out v1.1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ require 'will_paginate/array'
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-      @feed_items = Advertisement.all.paginate(page: params[:page], :per_page => 50, :order => "updated_at DESC")
+      @feed_items = Advertisement.all.paginate(page: params[:page], :per_page => 25, :order => "updated_at DESC")
     end
   end
 
