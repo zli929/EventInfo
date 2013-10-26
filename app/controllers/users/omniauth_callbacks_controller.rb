b8c952fc7ddf89e8a7910c9f6f345dbc8a76e83d
John Li <johnsli@msn.com>
2013-10-26 00:42:53 -0400

    fixing issues

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,15 +13,17 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
                                   request.env["omniauth.auth"].extra.raw_info.first_name, 
                                   request.env["omniauth.auth"].extra.raw_info.last_name, 
                                   request.env["omniauth.auth"].info.email, 
-                                  current_user)
+                                  current_user)                         
+      if @user.persisted?
+        sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+              
+      else
+        session["devise.omniauth_data"] = request.env["omniauth.auth"].uid
+        redirect_to new_user_registration_url
+      end
     end
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-            
-    else
-      session["devise.omniauth_data"] = request.env["omniauth.auth"].uid
+    else 
       redirect_to new_user_registration_url
     end
   end
